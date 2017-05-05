5183857767c4a217bd7ad92404b67c6481d77502
Hassler Thurston <jthurst3@u.rochester.edu>
2017-05-05 10:42:29 -0400

    add more data from node03-node06

--- timetodepth.sh
+++ timetodepth.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 # Output time it takes Stockfish to reach a certain depth.
 
-SECONDS=120
-BRANCHES="naive-tt irreversible-tt cache-tt"
+SECS=60
+BRANCHES="naive-tt irreversible-tt cache-tt cache-lock-tt global-smp"
 
-MILLISECONDS=$(expr $SECONDS \* 1000)
+MILLISECONDS=$(expr $SECS \* 1000)
 
 echo "time-to-depth testing started" >&2
-i=2
-while true; do
 
 	for branch in $BRANCHES; do
 		echo $branch
@@ -21,14 +19,18 @@ while true; do
 		cd ..
 
 		# run stockfish, need to keep pipe open
-		SECONDS=120
-		echo $branch $MILLISECONDS $i $SECONDS
-		(cat <<- EOF
-			go movetime $MILLISECONDS
-		EOF
-		sleep $SECONDS) | mpirun -np 2 -machinefile src/testhosts ./src/stockfish > "tmp-asdfasdfasdf.txt" 2> "data/${branch}_${i}.err"
-		./parseLine.py < "tmp-asdfasdfasdf.txt" > "data/${branch}_${i}.txt"
-		echo "finished with " $branch
+		for i in `seq 1 4`; do
+			SECS=60
+			MILLISECONDS=$(expr $SECS \* 1000)
+			echo $branch $MILLISECONDS $i $SECS
+			(cat <<- EOF
+				setoption name Threads value $i
+				go movetime $MILLISECONDS
+			EOF
+			sleep $SECS) | mpirun -np 4 -machinefile src/testhosts ./src/stockfish > "tmp-asdfasdfasdf.txt" 2> "data/${branch}_${i}.err"
+			./parseLine.py < "tmp-asdfasdfasdf.txt" > "data/${branch}_${i}.txt"
+			echo "finished with " $branch $i
+		done
 	done
 
 		branch="stockfish-master"
@@ -42,15 +44,17 @@ while true; do
 		cd ..
 
 		# run stockfish, need to keep pipe open
-		SECONDS=120
-		echo $branch $MILLISECONDS $i $SECONDS
-		(cat <<- EOF
-			go movetime $MILLISECONDS
-		EOF
-		sleep $SECONDS) | ./src/stockfish > "tmp-asdfasdfasdf.txt" 2> "data/${branch}_${i}.err"
-		./parseLine.py < "tmp-asdfasdfasdf.txt" > "data/${branch}_${i}.txt"
-		echo "finished with " $branch
+		for i in `seq 1 4`; do
+			SECS=60
+			MILLISECONDS=$(expr $SECS \* 1000)
+			echo $branch $MILLISECONDS $i $SECS
+			(cat <<- EOF
+				setoption name Threads value $i
+				go movetime $MILLISECONDS
+			EOF
+			sleep $SECS) | ./src/stockfish > "tmp-asdfasdfasdf.txt" 2> "data/${branch}_${i}.err"
+			./parseLine.py < "tmp-asdfasdfasdf.txt" > "data/${branch}_${i}.txt"
+			echo "finished with " $branch $i
+		done
 
 
-	i=$(( $i + 1 ))
-done
